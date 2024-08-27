1dd83d6f7dc7c15e2dcf8e211f78dfd790defbbb
Kaitlyn Bae <kaitlynbae07@gmail.com>
2024-08-27 09:39:09 -0500

    Initial Commit

---
+++ Persistance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Persistance
+//
+//  Created by Kaitlyn H. Bae on 8/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Persistance/PersistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistanceApp.swift
+//  Persistance
+//
+//  Created by Kaitlyn H. Bae on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PersistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
