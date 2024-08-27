a17ded81d935ac1b005e1461bd167ce202eddf0e
Kaitlyn Bae <kaitlynbae07@gmail.com>
2024-08-27 09:42:31 -0500

    added files

---
+++ Persistance/CollegeList.swift
@@ -0,0 +1,8 @@
+//
+//  CollegeList.swift
+//  Persistance
+//
+//  Created by Kaitlyn H. Bae on 8/27/24.
+//
+
+import Foundation
---
+++ Persistance/ToDoList.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoList.swift
+//  Persistance
+//
+//  Created by Kaitlyn H. Bae on 8/27/24.
+//
+
+import Foundation
