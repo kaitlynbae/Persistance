bec59ecd8b436375726fedacc4df3999259dbb95
Kaitlyn Bae <kaitlynbae07@gmail.com>
2024-09-03 09:43:45 -0500

    .

--- Persistance/CollegeList.swift
+++ Persistance/CollegeList.swift
@@ -5,17 +5,65 @@
 //  Created by Kaitlyn H. Bae on 8/27/24.
 //
 
-import Foundation
 import SwiftUI
+import SwiftData
+struct CollegeList: View {
 
-struct CollegeListView: View {
+
+    @Environment(\.modelContext) var context
+    @Query var college: [Person]
+    @State var enteredName = ""
     var body: some View {
-        VStack {
-            
+        
+        HStack{
+           
+            Text("College List")
+            .font(.custom("Verdana ",fixedSize:35))
+            Image(systemName: "graduationcap.fill")
+                .font(.largeTitle)
+                .padding()
         }
+        HStack{
+        TextField("Enter College Name", text: $enteredName)
+                .textFieldStyle(.roundedBorder)
+            
+            Button("+"){
+                let person = Person(name: enteredName)
+                context.insert(person)
+                enteredName = ""
+            }
+            .font(.largeTitle)
+            }
+        .padding()
+        List{
+            ForEach(college) {currentPerson in
+                Text(currentPerson.name)
+            }
         }
     }
+}
+
+@Model
+class Person {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
 
 #Preview {
-    CollegeListView()
+    CollegeList()
 }
+
+
+
+
+
+
+
+
+
+
+
